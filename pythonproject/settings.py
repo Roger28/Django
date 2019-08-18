02c29fbcc2edad6f792bb730c6b7c5cbd298d469
Roger28 <rogersouza635@gmail.com>
2019-08-18 16:19:39 -0300

    Criado projeto e tabela courses.

--- pythonproject/settings.py
+++ pythonproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'pythonproject.core',
+	'pythonproject.courses',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pythonproject', 'media')
+
+
+
